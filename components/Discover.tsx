b7b0b951f79065345af421f96fc1fae559adf52a
McVyp <93302780+McVyp@users.noreply.github.com>
2022-11-17 23:14:46 +0545

    added discover component

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { topics } from '../utils/constants'
+
 
 export const Discover = () => {
+
+    const router =useRouter();
+    const {topic} = router.query;
+    const activeTopicsStyle= 'xl:border-2 hover:bg-primary xl:border-pink-200 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer font-semibold text-pink-200'
+    const topicStyle='xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer font-semibold text-black'
   return (
-    <div>Discover</div>
+    <div className='xl:border-v-2 xl:border-gray-200 pb-6'>
+        <p className='text-gray-5000 font-semibold mt-4 m-3 hidden xl:block'>Popular Topics</p>
+        <div className='flex gap-3 flex-wrap'>
+            {topics.map((item)=>(
+                <Link href={`/?topic=${item.name}`} key={item.name}>
+                    <div className={topic === item.name? activeTopicsStyle : topicStyle}>
+                        <span className='font-bold text-2xl xl:text-md'>{item.icon}</span>
+                        <span className='hidden font-bold xl:block text-md capitalize'>{item.name}</span>
+                    </div>
+                </Link>
+            ))}
+        </div>
+
+    </div>
   )
 }
