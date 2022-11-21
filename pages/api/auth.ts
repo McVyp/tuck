c4bee885d347ec044759486fbb464e1bc9f41e2c
McVyp <93302780+McVyp@users.noreply.github.com>
2022-11-21 18:48:51 +0545

    uploading error

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -8,5 +8,6 @@ export default async function handler(req: NextApiRequest,res:NextApiResponse) {
     const user = req.body;
     client.createIfNotExists(user)
         .then(() => res.status(200).json('Login success'))
+        .catch(error => console.error(error))
  }
 }
--- utils/client.ts
+++ utils/client.ts
@@ -1,7 +1,7 @@
 import sanityClient from '@sanity/client';
 
 export const client = sanityClient({
-  projectId: '22k8bmk0',
+  projectId: '6qkiqyx9',
   dataset: 'production',
   apiVersion: '2022-03-10',
   useCdn: false,
