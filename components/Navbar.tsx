fe71ce3c4389376e70e13a14f6698a85954d058c
McVyp <93302780+McVyp@users.noreply.github.com>
2022-11-21 16:35:33 +0545

    added google log in and out

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,10 @@ import {BiSearch} from 'react-icons/bi';
 import {IoMdAdd} from 'react-icons/io';
 import logo from '../utils/tiktik-logo.png'
 import { createOrGetUser } from '../utils';
+import useAuthStore from '../store/authStore';
 
 export const Navbar = () => {
-  const user= false
+  const { userProfile, addUser, removeUser} = useAuthStore();
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
         <Link href={"/"}>
@@ -27,9 +28,41 @@ export const Navbar = () => {
           Search
         </div>
         <div>
-          {user ? (<div>logged in</div>):(
+          {userProfile ? (
+            <div className='flex gap-5 md:gap-10'>
+                <Link href={'/'}>
+                  <button className='border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2'>
+                    <IoMdAdd  className='text-xl'/> {` `}
+                    <span className='hidden md:block'>Upload</span>
+                  </button>
+                </Link>
+                {userProfile.image &&(
+                  <Link href={"/"}>
+                  {/* add <></> while putting images inside Link if error occurs */}
+                    <Image 
+                        height={40}
+                        width={40}
+                        className='rounded-full cursor-pointer'
+                        src={userProfile.image}
+                        alt="profile picture"
+  
+                    />
+                  </Link>
+                )}
+                <button
+                  type='button'
+                  className='px-2'
+                  onClick={() => {
+                    googleLogout();
+                    removeUser();
+                  }}
+                >
+                  <AiOutlineLogout color='red' fontSize={21} />
+                </button>
+            </div>
+            ):(
             <GoogleLogin 
-              onSuccess={(response) => createOrGetUser(response)}
+              onSuccess={(response) => createOrGetUser(response, addUser)}
               onError={() => console.log('Error')}
               />
           )}
